4adf8ed53bc9c3112efa5bccf9d98ad424e51f60
liyibo0103 <1771469353@qq.com>
2025-09-11 22:57:48 +0800

    day6

---
+++ Competition_day_six/src/Demo1.java
@@ -0,0 +1,15 @@
+public class Demo1 {
+    public int findMin(int[] nums) {
+        int left = 0,right = nums.length-1;
+        int x = nums[right];
+        while(left < right) {
+            int mid = (left + right) / 2;
+            if(nums[mid] > x) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+}
