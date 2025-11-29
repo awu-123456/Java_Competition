eb4c856838e887f478e7b5dc03dbdc31b0eba717
awu-123456 <1771469353@qq.com>
2025-11-29 22:44:12 +0800

    day73

---
+++ Competition/src/day73.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class day73 {
+    public boolean containsNearbyDuplicate(int[] nums, int k)
+    {
+        Map<Integer, Integer> hash = new HashMap<>();
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(hash.containsKey(nums[i]))
+            {
+                if(i - hash.get(nums[i]) <= k) return true;
+            }
+            hash.put(nums[i], i);
+        }
+        return false;
+    }
+}
